0547ff3f3e0d82372058dab4c1747573f4cac690
Mr Dk <562655624@qq.com>
2018-11-04 00:15:49 +0800

    Add Event bus

---
+++ src/main/java/iot/zjt/vertx/demo/eventbus/Entity.java
@@ -0,0 +1,32 @@
+package iot.zjt.vertx.demo.eventbus;
+
+public class Entity {
+
+    private String name;
+    private int age;
+    private double price;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
